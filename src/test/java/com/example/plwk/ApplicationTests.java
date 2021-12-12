0e26976acbc6aa7e227ab68a85e8a2db784ec204
Wiktoria Kulczyńska <wiktoriakulczynska@gmail.com>
2021-12-12 22:58:07 +0100

    Zadanie 20.1

---
+++ src/test/java/com/example/plwk/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.plwk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
